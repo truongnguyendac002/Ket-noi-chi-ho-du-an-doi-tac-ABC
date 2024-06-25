b2ed9876cabf1f0ea5fb1bb6e8719093c9569e2d
Truong <truongnguyendac002@gmail.com>
2024-06-25 10:02:20 +0700

    init

---
+++ src/main/java/com/msb/insurance/pob/PobApplication.java
@@ -0,0 +1,13 @@
+package com.msb.insurance.pob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msb/insurance/pob/PobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msb.insurance.pob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
