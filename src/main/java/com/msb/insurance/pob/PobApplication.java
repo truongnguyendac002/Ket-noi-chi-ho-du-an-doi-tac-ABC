79160c5bcd92cc8c745127cf65fa08632698ab82
Truong <truongnguyendac002@gmail.com>
2024-06-26 09:37:38 +0700

    tich hop sqlite

--- src/main/java/com/msb/insurance/pob/PobApplication.java
+++ src/main/java/com/msb/insurance/pob/PobApplication.java
@@ -1,13 +1,14 @@
 package com.msb.insurance.pob;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class PobApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PobApplication.class, args);
 	}
-
 }
